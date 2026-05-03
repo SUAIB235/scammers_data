a520bfb76284144feeea1f194ed24bd8da42f90c
suyeb00 <suyeb00@gmail.com>
2026-05-04 04:25:23 +0600

    fresh start project

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import CopyPaste from "./copypaste.jsx";
 import "./index.css";
+import CopyPaste from "./copypaste.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
